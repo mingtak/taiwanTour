bc898bd9dca63167e0342f6ef4bffb2b4b53f6a2
andy fang <andy@mingtak.com.tw>
2014-04-02 17:17:58 +0100

    backup , 0402

---
+++ twtour.content/twtour/content/attractions.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+from five import grok
+
+from z3c.form import group, field
+from zope import schema
+from zope.interface import invariant, Invalid
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from plone.dexterity.content import Container
+from plone.directives import dexterity, form
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.interfaces import IImageScaleTraversable
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+
+from twtour.content import MessageFactory as _
+from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget, AutocompleteFieldWidget
+from twtour.content.city import ICity
+
+
+# Interface class; used to define content-type schema.
+
+class IAttractions(form.Schema, IImageScaleTraversable):
+    """
+    Attractions for taiwan tour
+    """
+
+    form.widget(cityName=AutocompleteFieldWidget)
+    cityName = RelationChoice(
+        title=_(u"City Name"),
+        source=ObjPathSourceBinder(
+                object_provides=ICity.__identifier__,),
+        required=True,
+    )
+
+    contact = RichText(
+        title=_(u'contact infomation'),
+        required=False,
+    )
+
+    webSiteUrl = schema.URI(
+        title=_(u'website url'),
+        required=False,
+    )
+
+    introduction = RichText(
+        title=_(u'Introduction'),
+        required=False,
+    )
+
+    location = schema.TextLine(
+        title=_(u'Location'),
+        required=False,
+    )
+
+    address = schema.TextLine(
+        title=_(u'Address'),
+        required=False,
+    )
+
+    headImage = NamedBlobImage(
+        title=_(u'head Image'),
+        required=True,
+    )
+
+    image1 = NamedBlobImage(
+        title=_(u'Image1'),
+        required=False,
+    )
+
+    image2 = NamedBlobImage(
+        title=_(u'Image2'),
+        required=False,
+    )
+
+    image3 = NamedBlobImage(
+        title=_(u'Image3'),
+        required=False,
+    )
+
+    image4 = NamedBlobImage(
+        title=_(u'Image4'),
+        required=False,
+    )
+
+    copyrightMark = schema.Bool(
+        title=_(u'copyright mark'),
+        description=_(u'If image copyright OK, please checked the box'),
+        required=True,
+    )
+
+#validator, 同性質的可以綁在一起共用同一個validator
+@form.validator(field=IAttractions['headImage'])
+@form.validator(field=IAttractions['image1'])
+@form.validator(field=IAttractions['image2'])
+@form.validator(field=IAttractions['image3'])
+@form.validator(field=IAttractions['image4'])
+def validateImage(image):
+    if not hasattr(image, 'size'):
+        return
+    if image._width > 500:
+        raise Invalid(_(u'Image width over 500px'))
+    if image._height > 500:
+        raise Invalid(_(u'Image height over 500px'))
+    if image.size > 512000:
+        raise Invalid(_(u'Image size over 512KB'))
+
+
+class Attractions(Container):
+    grok.implements(IAttractions)
+
+
+class SampleView(grok.View):
+    """ sample view class """
+
+    grok.context(IAttractions)
+    grok.require('zope2.View')
+
+    # grok.name('view')
+
+    # Add view methods here
---
+++ twtour.content/twtour/content/city.py
@@ -0,0 +1,60 @@
+from five import grok
+
+from z3c.form import group, field
+from zope import schema
+from zope.interface import invariant, Invalid
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from plone.dexterity.content import Container
+from plone.directives import dexterity, form
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.interfaces import IImageScaleTraversable
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from twtour.content import MessageFactory as _
+
+from plone.indexer import indexer
+
+
+regionList = SimpleVocabulary(
+    [SimpleTerm(value=u'Northern', title=_(u'Northern')),
+     SimpleTerm(value=u'Central', title=_(u'Central')),
+     SimpleTerm(value=u'Southern', title=_(u'Southern')),
+     SimpleTerm(value=u'Eastern', title=_(u'Eastern')),
+     SimpleTerm(value=u'Outlying Islands', title=_(u'OutlyingIslands')),]
+    )
+
+
+class ICity(form.Schema, IImageScaleTraversable):
+    """
+    Cities Infomation
+    """
+    region = schema.Choice(
+            title=_(u"Region"),
+            vocabulary=regionList,
+            required=True,
+        )
+
+
+class City(Container):
+    grok.implements(ICity)
+
+
+class SampleView(grok.View):
+    """ sample view class """
+
+    grok.context(ICity)
+    grok.require('zope2.View')
+
+    # grok.name('view')
+
+
+@indexer(ICity)
+def region_indexer(obj):
+     return obj.region
+grok.global_adapter(region_indexer, name='region')
